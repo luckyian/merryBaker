1387b797b1e35b8a8ef56a77c9193e40e8712ba1
Ian Wren <ian.wren81@gmail.com>
2022-07-10 18:36:01 -0700

    Changed all css files to have new color scheme, removed band info and changed file folder names for new structure

--- src/components/Subcomponents/Goods/index.js
+++ src/components/Subcomponents/Goods/index.js
@@ -27,49 +27,6 @@ function Goods() {
          </h4>
          
          <br></br>
-         <br></br>
-         <br></br>
-         <h4>July 15th, 2022</h4>
-         <h4>
-         <a href="https://www.facebook.com/PTAnchor/">The Anchor Port</a>
-         </h4>
-         <h4>Port Townsend, WA</h4>
-         {/* <h4>
-           <a href="https://concrete-wa.com/events/fly-in/">Concrete Fly In</a>
-          </h4> */}
-         <br></br>
-         <br></br>
-         <br></br>
-         <h4>July 16th, 2022</h4>
-         <h4>
-           <a href="https://concrete-wa.com/events/fly-in/">Concrete Fly In</a>
-          </h4>
-         <h4>Concrete, WA</h4>
-         
-         <br></br>
-         <br></br>
-         <br></br>
-         <h4>August 27th, 2022 @ 8pm</h4>
-         <h4>
-         <a href="https://highdiveseattle.com/">High Dive</a>
-         </h4>
-         <h4>Seattle, WA</h4>
-         <h4>
-          {/* <a href="https://highdiveseattle.com/e/calendar/">Concerts on the Dock</a> */}
-         </h4>
-         <br></br>
-         <br></br>
-         <br></br>
-         <h4>Semptember 3th, 2022 @ 7pm</h4>
-         <h4>
-          <a href="http://www.eaglehavenwinery.com/summer-concerts.html">Summer Concerts</a>
-         </h4>
-         
-         <h>Outdoor Pavilion</h>
-         <h4>
-         <a href="http://www.eaglehavenwinery.com/">Eagle Haven Winery</a>
-         </h4>
-         <h4>Sedro-Woolley, WA</h4>
          
         
               
