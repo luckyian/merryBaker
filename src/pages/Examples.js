da4c6cd9be68e2dadd79063804deb9d2c77a4c60
Ian Wren <ian.wren81@gmail.com>
2022-08-22 14:39:27 -0700

    Changed Video page to Example page to showcase cupcakes

---
+++ src/pages/Examples.js
@@ -0,0 +1,56 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import example1 from "./merryGnomeClose.jpg";
+import example2 from "./merryGnomeCookie.jpg";
+import example3 from "./merryGnomeFish.jpg";
+import example4 from "./merryOreoSundae.jpg";
+import example5 from "./merryPeachCobbler.jpg";
+
+function Examples() {
+
+
+  return (
+
+    <Container>
+      <br/>
+      <Card>
+
+
+      <Card.Img src={example1} alt="TheGoods"></Card.Img>
+
+      </Card>
+      <br/>
+      <Card className="video-responsive">
+
+      <Card.Img src={example4} alt="TheGoods"></Card.Img>
+
+      </Card >
+<br/>
+      <Card className="video-responsive">
+
+      <Card.Img src={example2} alt="TheGoods"></Card.Img>
+      </Card >
+      <br/>
+      <Card className="video-responsive">
+
+      
+      <Card.Img src={example3} alt="TheGoods"></Card.Img>
+      
+      </Card >
+      <br/>
+      <Card className="video-responsive">
+
+      <Card.Img src={example5} alt="TheGoods"></Card.Img>
+      </Card >
+      
+      
+
+    </Container >
+
+  );
+
+}
+
+export default Examples;
