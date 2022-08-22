6f45c1cefc6b69665b2aa414bb37d5e8787a465c
Ian Wren <ian.wren81@gmail.com>
2022-08-22 14:29:08 -0700

    Changed route names to match client/site's theme and content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Index from "./pages/Index";
-import Videos from "./pages/Videos";
+import Examples from "./pages/Examples";
 import Navibar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -20,7 +20,7 @@ function App() {
           <Route exact path="/" component={Index} />
           <Route exact path="/index" component={Index} />
           <Route exact path="/contact" component={Contact} />
-          <Route exact path="/videos" component={Videos} />
+          <Route exact path="/cakes" component={Examples} />
 
         </Wrapper>
         <Footer />
