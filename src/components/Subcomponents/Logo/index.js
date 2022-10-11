7a68de9352e7a8a5d56af451324571a2e9839013
luckyian <ian.wren81@gmail.com>
2022-10-10 21:09:06 -0700

    Changed logo to new logo desired by client

--- src/components/Subcomponents/Logo/index.js
+++ src/components/Subcomponents/Logo/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import "./style.css";
-import logo from "./merryLogo2.jpg";
+import logo from "./merryGnomeLogo.jpg";
 
 
 function Logo() {
