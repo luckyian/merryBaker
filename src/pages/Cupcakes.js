629b9fb5e7ec298559000b34a59969bae405b728
luckyian <ian.wren81@gmail.com>
2023-10-14 23:10:21 -0700

    Removed old cup cake pictures and added new cupcake pictures

--- src/pages/Cupcakes.js
+++ src/pages/Cupcakes.js
@@ -2,19 +2,21 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import example1 from "./cupcake1.jpeg";
-import example2 from "./cupcake2.jpeg";
-import example3 from "./cupcake3.jpeg";
+import example1 from "./cupCake1.jpeg";
+import example2 from "./cupCake2.jpeg";
+import example3 from "./cupCake3.jpeg";
 import example4 from "./cupcake4.jpeg";
-import example5 from "./cupcake5.jpeg";
-import example6 from "./cupcake6.jpeg";
-import example7 from "./cupcake7.jpeg";
-import example8 from "./cupcake8.jpeg";
-import example9 from "./cupcake9.jpeg";
-import example10 from "./cupcake10.jpeg";
-import example11 from "./cupcake11.jpeg";
-import example12 from "./cupcake12.jpeg";
-import example13 from "./cupcake13.jpeg";
+import example5 from "./cupCake5.jpeg";
+import example6 from "./cupCake6.jpeg";
+import example7 from "./cupCake7.jpeg";
+import example8 from "./cupCake8.jpeg";
+import example9 from "./cupCake9.jpeg";
+import example10 from "./carmelCupCake.jpeg";
+import example11 from "./chocolateBarCupCake.jpeg";
+import example12 from "./cookieCupCake.jpeg";
+import example13 from "./penguinCupCake.jpeg";
+import example14 from "./truckCupCake.jpeg";
+import example15 from "./barbieCupCake.jpeg";
 
 function Cupcakes() {
 
@@ -101,6 +103,18 @@ function Cupcakes() {
       <Card>
 
       <Card.Img src={example13} alt="TheGoods"></Card.Img>
+      </Card >
+
+      <br/>
+      <Card>
+
+      <Card.Img src={example14} alt="TheGoods"></Card.Img>
+      </Card >
+
+      <br/>
+      <Card>
+
+      <Card.Img src={example15} alt="TheGoods"></Card.Img>
       </Card >
       
       <h1 className="text3">I do NOT ship</h1>
