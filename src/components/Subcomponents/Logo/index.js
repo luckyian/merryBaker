4d4c14b3d63592f3bd571c386278b230a34c8733
luckyian <ian.wren81@gmail.com>
2025-03-26 11:49:56 -0700

    Changed logo on front page

--- src/components/Subcomponents/Logo/index.js
+++ src/components/Subcomponents/Logo/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import "./style.css";
-import logo from "./logoNew.jpeg";
+import logo from "./logo3.jpeg";
 
 
 function Logo() {
