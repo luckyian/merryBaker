2fb39a3bfb0c50058352268ad4c4cc734ea2d681
luckyian <ian.wren81@gmail.com>
2025-03-26 13:26:37 -0700

    Deleted old pics of cakes and replaced with karens place logo and added text description of Karens place

--- src/components/Subcomponents/Goods/index.js
+++ src/components/Subcomponents/Goods/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import "./style.css";
-import frontpage from "./cake1.jpeg"
+import frontpage from "./karensPlaceWords.jpeg"
 import ending from "./merryLogo2.jpeg"
 
 
@@ -12,28 +12,23 @@ function Goods() {
 
     return (
         <Card className="aboutBack">
+          
         <Card.Body className="text">
           
           <div className="tourWords2">
-          <h1 className="tourWords">Cakes</h1>
+          
             <Card.Img src={frontpage} alt="TheGoods"></Card.Img>
+            
           </div>
           <div>
          <p className="tourWords">
-
+         <h1 className="tourWords">Karen's Place is the home Merry Little Baker who sells macarons, cupcakes, 
+            custom orders and other treats. It's also a spot for local small businesses to sell their porducts and
+            a commisary kitchen.
+            </h1>
            {/* <h3>TBD</h3> */}
            {/* <h3>Check Back Soon for Upcoming Shows</h3> */}
-         <h1 className="tourWords">Cupcakes</h1>
-         <br></br>
-         <h4>
-          <Card.Img src={ending} alt="Piggy"></Card.Img> 
-         </h4>
-         
-         <br></br>
-         
         
-              
-             
 
           </p>
 
