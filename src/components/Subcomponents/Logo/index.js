a51bcaa2860938ba948ff78279e5966147cdda02
luckyian <ian.wren81@gmail.com>
2025-03-26 20:43:48 -0700

    Added text and the picture of the ribbon cutting to the Merry Little Baker portion of the landing page

--- src/components/Subcomponents/Logo/index.js
+++ src/components/Subcomponents/Logo/index.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import "./style.css";
 import logo from "./logo3.jpeg";
+import ribbon from "./ribbonCutting.jpeg"
 
 
 function Logo() {
@@ -16,6 +17,16 @@ function Logo() {
                 <div >
                     <Card.Img src={logo}  alt="MerryLittleBaker"></Card.Img>
                 </div>
+                <Card>
+                <p className="tourWords">
+         <h1 className="tourWords">Merry Little Baker works and sells it's goods out of Karen's Place in
+            Stanwood, WA.  They opened the store on February 28th, 2025.
+            </h1>
+           </p>
+           </Card>
+           <div>
+           <Card.Img src={ribbon}  alt="RibbonCutting"></Card.Img>
+           </div>
             </Card.Body>
         </Card>
     )
