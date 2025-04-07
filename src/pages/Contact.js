09601875f06286f401d93d81be8ea4659c1e6344
luckyian <ian.wren81@gmail.com>
2025-04-06 21:47:49 -0700

    Created new card at top of menu pages to give contact information on ordering and added a hyperlink to google maps location

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -16,6 +16,21 @@ function Contact() {
     <div>
       <div className="mt-5">
         <h1 className="text2">Order</h1>
+        <Card className="aboutBack">
+          <Card.Body className="text">
+          <div className="boxer">
+              
+              <h4 className="fontSize">Order:</h4>
+              <h3>Email:</h3>
+              <h4 className="fontSize">Order@MerryLittleBaker.com</h4>
+              <br></br>
+              <div className="boxer">
+              <h3>Phone:</h3>
+              <h4 className="fontSize">425-308-1663</h4>
+            </div>
+            </div>
+          </Card.Body>
+        </Card>
         <Menu />
 
         <Card className="aboutBack">
@@ -53,7 +68,7 @@ function Contact() {
             <div className="boxer">
               <h3>Pick-up Location:</h3>
               <h4 className="fontSize">8701 271st NW Stanwood, WA 98292</h4>
-            {/* https://maps.app.goo.gl/kesL3QZvysZC2AhK8 */}
+             <a href="https://www.google.com/maps/place/Merry+Little+Baker/@48.2430576,-122.3537432,14.75z/data=!4m16!1m9!3m8!1s0x54855c68d27f92b1:0xab96424e07978b9!2s8701+271st+St+NW,+Stanwood,+WA+98292!3b1!8m2!3d48.2424353!4d-122.350608!10e3!16s%2Fg%2F11bw3yd1x0!3m5!1s0x54855d6b11916ca3:0x62de7da646df5ed9!8m2!3d48.2424353!4d-122.350608!16s%2Fg%2F11m5q1qqts?entry=ttu&g_ep=EgoyMDI1MDQwMi4xIKXMDSoASAFQAw%3D%3D">Google Maps Location</a>
             
             </div>
           </Card.Body>
